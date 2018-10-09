ec8d1472ef5bc130bb3b6f3125c2eabb6457e8c3
Rui Pinge <rui.pinge@elgin.org.uk>
2018-10-09 22:32:33 +0100

    WIP

---
+++ tools/publish.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pushd "$(dirname "$0")"
+echo $PWD
+popd
--- www/js/util.js
+++ www/js/util.js
@@ -7,6 +7,7 @@ define([
     return {
 
         stringClean: function(s) {
+            // TODO: replace multiple spaces with just one
             return _.trim(s).replace(/(?:\r\n|\r|\n)/g, ' ');
         }
 
