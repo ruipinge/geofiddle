a73c0b072345c81744dabc4f24c879bcfb62c869
Rui Pinge <rui.pinge@elgin.org.uk>
2018-10-09 23:43:13 +0100

    GitHub Pages publish script.

--- tools/build.js
+++ tools/build.js
@@ -2,7 +2,7 @@
     "name": "main",
     "mainConfigFile": "../www/js/main.js",
     "include": "main",
-    "out": "../www/main.min.js",
+    "out": "../main.min.js",
     "wrap": true,
     "baseUrl": "../www/js",
     "preserveLicenseComments": false,
--- tools/publish.sh
+++ tools/publish.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
+# Publishes (or deploys) GeoFiddle site to GitHub Pages using gh-pages branch.
 
-pushd "$(dirname "$0")"
-echo $PWD
+# Makes script exit when subsequent commands fail
+set -e
+
+export PUBLISH_BRANCH=gh-pages
+export CACHE_BUSTER="$(date +%s)"
+export SRC_DIR="./www"
+export DST_DIR="./"
+
+pushd "$(dirname "$0")"/..
+
+# Reset and checkout GitHub Pages branch
+git branch -C $PUBLISH_BRANCH
+git checkout $PUBLISH_BRANCH
+
+# Copy relevant (static) files
+cp $SRC_DIR/*.* $DST_DIR
+cp -r $SRC_DIR/css $DST_DIR
+
+# Generate main.min.js JavaScript minified file
+npm run build
+
+# Remove development JavaScript file reference
+sed -i '' -e 's/^.*JS Development version.*$//' $DST_DIR/index.html
+
+# Uncomment production/minified JavaScript file reference
+sed -i '' -e 's/^.*JS Production version \(.*\) -->.*$/  \1/' $DST_DIR/index.html
+
+# Deploy by adding and pushing
+git add $DST_DIR
+git commit -m"Build $CACHE_BUSTER publish."
+git push -f origin $PUBLISH_BRANCH
+
 popd
