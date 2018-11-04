ec94016ab37756108c7ede9e4c00449af20f11c6
Rui Pinge <rui.pinge@elgin.org.uk>
2018-11-04 21:18:11 +0000

    Webpack Hot Module Replacement (HMR) support for development.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,18 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const webpack = require('webpack');
 
 module.exports = merge(common, {
     mode: 'development',
     output: {
         filename: '[name].js'
     },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
     devServer: {
-        contentBase: path.join(__dirname, 'www')
+        contentBase: path.join(__dirname, 'www'),
+        hot: true
     }
 });
