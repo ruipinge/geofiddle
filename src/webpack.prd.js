9a9b5ac7bb1d01fda9da859490fc541db1c2e2be
Rui Pinge <rui.pinge@elgin.org.uk>
2018-12-10 01:54:28 +0000

    Last GitHub Pages deploy try.

--- src/webpack.prd.js
+++ src/webpack.prd.js
@@ -6,7 +6,7 @@ module.exports = merge(common, {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
-        publicPath: '/dist/'
+        publicPath: 'dist/'
     },
     plugins: [
 
